78434802ce4133ec6d3622f539159cb50dfa185c
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-13 22:03:09 +0300

    Add RestaurantPagerAdapter to RestaurantDetailActivity

--- app/src/main/java/ui/RestaurantDetailActivity.java
+++ app/src/main/java/ui/RestaurantDetailActivity.java
@@ -1,15 +1,38 @@
 package ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
 import com.example.myrestaurants.R;
 
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import adapters.RestaurantPagerAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import models.Business;
+
 public class RestaurantDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private RestaurantPagerAdapter adapterViewPager;
+    List<Business> mRestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+        ButterKnife.bind(this);
+
+        mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new RestaurantPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mRestaurants);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
