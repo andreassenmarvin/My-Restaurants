da88be1c5de9637fd0dfed5f1047ea46c0042eaa
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-20 15:16:15 +0300

    Create user login activity

---
+++ app/src/main/java/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myrestaurants.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
